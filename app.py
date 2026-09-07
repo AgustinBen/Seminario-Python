78da9e627b93d3a480d6f6b3a618889936363d27
AgustinBen <agustinbengolea97@gmail.com>
2026-09-07 16:47:00 -0300

    avance pre-practica

--- app.py
+++ app.py
@@ -1,20 +1,41 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, HTTPException, status, Depends
+from database import Base, engine, get_db
+from sqlalchemy.orm import Session
+import models
+from models import Product
 
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", tags=["Home"])
 def hello_world():
     return {"message": "¡Hola, mundo!"}
 
 
 @app.get("/products", status_code=status.HTTP_200_OK)
-def get_products():
-    return {"message" : "Lista de Productos"}
+def get_products(db: Session = Depends(get_db)):
+    try:
+        products = db.query(Product).all()
+        response = {"products" : products}
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail= f"Error en /products: {e}")
+        
+
 
 @app.get("/products/{id}", status_code=status.HTTP_200_OK)
-def get_product(id: int):
-    return {"message" : f"Producto con id {id}"}
+def get_product(id: int, db: Session = Depends(get_db)):
 
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    
+    try:
+        product = db.query(Product).filter(Product.id == id).first()
+
+        response = {"product" : product}
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail= f"Error en /products/{id}: {e}")
 
 
 
--- database.py
+++ database.py
@@ -5,3 +5,17 @@ engine = create_engine("sqlite:///./database.db", connect_args={"check_same_thre
 
 Base = declarative_base()
 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+""" 
+autocommit=False: los cambios no se guardan automáticamente.
+autoflush=False: SQLAlchemy no envía cambios automáticamente antes de cada consulta.
+bind=engine: conecta la fábrica con SQLite. 
+"""
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db  # la función se pausa y "entrega" esa sesión (db) a quien la llamó. FastAPI la recibe y se la pasa al endpoint.
+    finally:
+        db.close()  # Una vez que el endpoint termina de ejecutarse, la función se reanuda y cierra la sesión (db).
